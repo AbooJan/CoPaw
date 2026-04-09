d22043152ae68d21bf712cf3de30661b7a9c4376
zhongbaojian <zhongbaojian@fnnas.com>
2026-04-09 11:19:26 +0800

    feat: update version

--- setup_python.sh
+++ setup_python.sh
@@ -2,4 +2,4 @@ python3 -m venv .env
 source .env/bin/activate
 
 # 本地安装 whl 包
-# pipx install ./dist/copaw-1.1.0-py3-none-any.whl  --python /opt/homebrew/opt/python@3.13/bin/python3.13 --force
+# pipx reinstall ./dist/copaw-1.1.0-py3-none-any.whl  --python /opt/homebrew/opt/python@3.13/bin/python3.13 --force
